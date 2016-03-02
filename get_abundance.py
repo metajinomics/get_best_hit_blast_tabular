916c33978294896134e7b0bb17837ef9d3993536
Jin Choi <jinchoi721@gmail.com>
2016-03-02 09:53:44 -0600

    update

--- get_abundance.py
+++ get_abundance.py
@@ -20,5 +20,5 @@ for line in sumread:
 
 for line in blastread:
     spl = line.strip().split('\t')
-    if(dict.has_key(spl[1])):
-           print spl[1]+'\t'+dict[spl[1]]
+    #if(dict.has_key(spl[1])):
+           #print spl[1]+'\t'+dict[spl[1]]
